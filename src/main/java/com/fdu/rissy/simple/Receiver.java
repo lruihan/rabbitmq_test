9ed39e4c9cae5f63575d0dd1ef9d6bcafb6f81d0
Rissy Lin <lrissy@vmware.com>
2017-10-31 15:12:52 +0800

    first RabbitMQ example
    Only one sender & one receiver
    
    using spring amqp-client

---
+++ src/main/java/com/fdu/rissy/simple/Receiver.java
@@ -0,0 +1,41 @@
+package com.fdu.rissy.simple;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver {
+
+    private static final String QUEUE_NAME = "hello";
+
+    //private static Logger logger = LoggerFactory.getLogger(Sender.class);
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
---
+++ src/main/java/com/fdu/rissy/simple/Sender.java
@@ -0,0 +1,31 @@
+package com.fdu.rissy.simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private static final String QUEUE_NAME = "hello";
+
+    private static Logger logger = LoggerFactory.getLogger(Sender.class);
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("[x] send '" + message + "'");
+        channel.close();
+        connection.close();
+    }
+}
